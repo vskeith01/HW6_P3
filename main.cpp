1361d561e4e1e4a02f4f288875dd9b59dd4387b8
vskeith <111769909+vskeith01@users.noreply.github.com>
2022-10-02 22:42:04 +0000

    initialized variblaes for part a

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+  return 0;
+}
+
+
+
+/*  Function to calculate Power with unputs distance and water flow*/
+
+
+double PowerProduced(double H, double Q)
+{
+  double g = 9.81;    // m/s
+  double p = 1;   // Water density
+  double Power;   // Power Produced
+  
+}
